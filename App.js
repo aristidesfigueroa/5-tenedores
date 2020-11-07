dd03293ef8e0b1ea2b49bb7e05208346848051d8
Domingo <aristides@itsbyte.com>
2020-11-07 02:51:17 -0600

    Navigation Montado

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
+
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './app/navigations/Navigation';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola Mundo con EXPLO Client @aristidesfigueora</Text>      
-      <StatusBar style="auto" />
-    </View>
+    <Navigation />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
