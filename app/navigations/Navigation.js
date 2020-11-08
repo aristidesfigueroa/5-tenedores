16a3df8df7cc9435c9eb56ad908a350df2448aac
Domingo <aristides@itsbyte.com>
2020-11-07 23:35:47 -0600

    V.1.2 react-native-elements

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,8 +1,9 @@
 
-
+// NAVIGATION - BARRA DE ABAJO 
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from 'react-native-elements'; 
 
 import RestaurantsStack from "./RestaurantsStack";
 import TopRestaurantsStack from "./TopRestaurantsStack";
@@ -16,7 +17,16 @@ export default function Navigation() {
 
     return(
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+             initialRouteName="restaurants"
+             tabBarOptions={{
+                 inactiveTintColor: "#646464",
+                 activeTintColor: "#00a680",
+             }}
+             screenOptions={({ route }) => ({
+                 tabBarIcon: ({ color }) => screenOptions(route, color),
+             })}
+            >
                 <Tab.Screen 
                 name="restaurants" 
                 component={RestaurantsStack}
@@ -45,5 +55,34 @@ export default function Navigation() {
             </Tab.Navigator>
         </NavigationContainer>
         
+    );
+}
+
+function screenOptions(route, color) {
+    let iconName;
+    
+    switch (route.name) {
+        case "restaurants":
+            iconName = "compass-outline";            
+            break;
+        case "favorites":
+            iconName = "heart-outline";            
+            break;
+        case "topRestaurants":
+            iconName = "star-outline";            
+            break;
+        case "search":
+            iconName = "magnify";            
+            break;
+        case "account":
+            // iconName = "home-outline";
+            iconName = "account";            
+            break;
+    
+        default:
+            break;
+    }
+    return(
+        <Icon type="material-community" name={iconName}  size={22} color={color} />
     );
 }
