06a06f997615c6766e60ccfeeb11cf09ba5f8ecd
Domingo <aristides@itsbyte.com>
2020-11-09 02:33:53 -0600

    V.1.6 Creado Componente Loading

---
+++ app/components/Loading.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {StyleSheet, View, Text, ActivityIndicator} from "react-native";
+import { Overlay } from 'react-native-elements';
+
+export default function Loading( props ) {
+    
+    const { isVisible, text } = props;
+    return(
+        <Overlay
+        isVisible = {isVisible}
+        windowBackgroundColor = "rgba(0, 0, 0, 0.5)"
+        overlayBackgroundColor ="tranparent"
+        overlayStyle={styles.overlayMy}
+
+        >
+            <View style={styles.viewMy} >
+                <ActivityIndicator size="large" color="#00a680" />
+                {text && <Text style={styles.textMy} >{text}</Text>}                
+            </View>
+        </Overlay>
+    )
+}
+
+// Creando estilos
+
+const styles = StyleSheet.create({
+    overlayMy: {
+        height: 100,
+        width: 200,
+        backgroundColor: "#fff",
+        borderColor: "#00a680",
+        borderWidth:2,
+        borderRadius:10,
+    },
+    viewMy: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",            
+    },
+    textMy: {
+        color: "#00a680",
+        textTransform: "uppercase",
+        marginTop: 10,
+    },
+
+});
