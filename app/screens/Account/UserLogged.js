a938066a0f2dee28544e0f235ab58a03c52157da
Domingo <aristides@itsbyte.com>
2020-11-12 04:13:57 -0600

    V.1.66 UserLogged

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -2,41 +2,61 @@
 // si el usuario este no esta logeado, en vez de venir acá lo manda a UserGuest.js
 
 // #1 como ya registramos usuario en FireBase, ahora vamos a deslogear el usuario
+// OJO: Digamos el BUTTON lo podemos importar de react-native, pero acá lo importamos de 
+// react-native-elements porque tiene mas propiedades
 
-import React from "react";
-import {StyleSheet, View, Text, Button} from "react-native";
+
+import React, {useRef, useState } from "react";
+import {StyleSheet, Text, View } from "react-native";
+import { Button } from 'react-native-elements';
 import * as firebase from "firebase";
+import Toast from "react-native-easy-toast";
+
+import Loading from "../../components/Loading";
 
 export default function UserLogged() {
 
+    const refToast = useRef();
+    const [loading, setLoading] = useState(false); // para controlar Loading.js
+    const [loadingText, setLoadingText] = useState(""); // para controlar Loading.js
+
     return(
         <View style={styles.formContainer} >
-            <Text>UserLogged...</Text>
+            <Text>InfoUser...</Text>
+            <Text>AccountOptions...</Text>
             <Button 
             title="Cerrar Sesión"
-            onPress={() => firebase.auth().signOut()}
-            buttonStyle={styles.myBtn}
-            containerStyle={styles.myContainer}
-             />
+            buttonStyle={styles.myCloseSesion}
+            titleStyle={styles.myCloseSesionStyle}
+            onPress={() => firebase.auth().signOut()} // Cerrar Sesisión            
+            />
+            <Toast ref={refToast} position="center" opacity={0.9} />
+            <Loading isVisible={loading} text={loadingText} />
         </View>
-    )
+        
+    );
 }
 
 
 // Creando constante de estilos de nuestra screen "OJO StyleSheey.create" 
 const styles = StyleSheet.create({   
+
     formContainer: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop:30,
-    },     
-    myBtn: {
-        backgroundColor: "#00a680",
-    },   
-    myContainer: {
+        minHeight: "100%",
+        backgroundColor: "#f2f2f2",        
+    },    
+    myCloseSesion: {
         marginTop: 20,
-        width: "95%",                
+        borderRadius: 0,
+        backgroundColor: "#fff", 
+        borderTopWidth: 1,
+        borderTopColor: "#e3e3e3",  
+        paddingTop: 10,
+        paddingBottom: 10,
+
+    },    
+    myCloseSesionStyle: {
+        color: "#00a680",
     },
 
 });
